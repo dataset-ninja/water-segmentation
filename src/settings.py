614aa60fea17fdb431e3fa483608f5517df42017
Alex Erastov <123257559+erastov-alex@users.noreply.github.com>
2023-10-06 14:19:25 +0300

    reset preview

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/gvclsu/water-segmentation-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 6587491
+PREVIEW_IMAGE_ID: int = 6589319
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/water-segmentation"
